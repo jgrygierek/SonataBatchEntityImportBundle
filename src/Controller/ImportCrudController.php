d8ed04c4310f0a10d0aeb2d2020a4bb5e08f3e75
Jarosław Grygierek <jaroslawgrygierek@gmail.com>
2021-11-21 23:05:00 +0100

    make import configuration a service

--- src/Controller/ImportCrudController.php
+++ src/Controller/ImportCrudController.php
@@ -4,14 +4,37 @@ declare(strict_types=1);
 
 namespace JG\SonataBatchEntityImportBundle\Controller;
 
+use JG\BatchEntityImportBundle\Controller\ImportConfigurationAutoInjectInterface;
+use JG\BatchEntityImportBundle\Model\Configuration\ImportConfigurationInterface;
 use JG\SonataBatchEntityImportBundle\Admin\AdminWithImportInterface;
 use Sonata\AdminBundle\Admin\AdminInterface;
 use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+use UnexpectedValueException;
 
 /**
  * @property AdminInterface|AdminWithImportInterface $admin
  */
-class ImportCrudController extends CRUDController
+class ImportCrudController extends CRUDController implements ImportConfigurationAutoInjectInterface
 {
+    use ImportConfigurationAutoInjectTrait;
     use ImportControllerTrait;
+
+    protected function getImportConfiguration(): ImportConfigurationInterface
+    {
+        if (!$this->importConfiguration) {
+            $class = $this->getImportConfigurationClassName();
+            if (!class_exists($class)) {
+                throw new UnexpectedValueException('Configuration class not found.');
+            }
+
+            if (!isset($this->importConfigurations[$class])) {
+                throw new ServiceNotFoundException($class);
+            }
+
+            $this->importConfiguration = $this->importConfigurations[$class];
+        }
+
+        return $this->importConfiguration;
+    }
 }
