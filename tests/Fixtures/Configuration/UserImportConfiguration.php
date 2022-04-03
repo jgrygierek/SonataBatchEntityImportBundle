56c77c3fea06b31f78bcc38120c3f353b3c18a12
Jarosław Grygierek <jaroslawgrygierek@gmail.com>
2022-04-03 16:41:12 +0200

    add info about unique entity validator

--- tests/Fixtures/Configuration/UserImportConfiguration.php
+++ tests/Fixtures/Configuration/UserImportConfiguration.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace JG\SonataBatchEntityImportBundle\Tests\Fixtures\Configuration;
 
 use JG\BatchEntityImportBundle\Model\Configuration\AbstractImportConfiguration;
+use JG\BatchEntityImportBundle\Validator\Constraints\DatabaseEntityUnique;
+use JG\BatchEntityImportBundle\Validator\Constraints\MatrixRecordUnique;
 use JG\SonataBatchEntityImportBundle\Tests\Fixtures\Entity\User;
 
 class UserImportConfiguration extends AbstractImportConfiguration
@@ -12,5 +14,13 @@ class UserImportConfiguration extends AbstractImportConfiguration
     public function getEntityClassName(): string
     {
         return User::class;
+    }
+
+    public function getMatrixConstraints(): array
+    {
+        return [
+            new DatabaseEntityUnique(['entityClassName' => $this->getEntityClassName(), 'fields' => ['name']]),
+            new MatrixRecordUnique(['fields' => ['name']]),
+        ];
     }
 }
